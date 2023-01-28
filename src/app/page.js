e520068b869e73ae69f14b3c8d4b7f2c98d76dc1
AbdessamadPas <boughadoin@gmail.com>
2023-01-28 18:23:10 +0100

    setup 💕

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,70 @@
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import Image from 'next/image';
+import deved from '../../public/dev-ed-wave.png';
+import consulting from '../../public/consulting.png';
+import design from '../../public/design.png';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header>
+        <title>wewe</title>
+      </header>
+      <main className='bg-white px-10  md:px-20 lg:px-40'>
+      <section className=' min-h-screen'>
+        <nav className='flex justify-between	mb-12 p-10'>
+          <h1 className='text-xl font-burtons'> AbdessamadPas</h1>
+          <ul className='flex  items-center'>
+            <li ><BsFillMoonStarsFill className='cursor-pointer	text-xl'/></li>
+            <li>
+              <a className='rounded-md px-4 py-2 ml-10  bg-gradient-to-r from-cyan-500 to-blue-500 ' >resume</a></li>
+          </ul>
+        </nav>
+        <div className='text-center py-10'>  
+          <h2 className='text-5xl py-3 text-teal-600'> Abdessamad AALOUWAN</h2>
+          <h3 className='text-2xl py-2 text-gray-600'> Full stack developer</h3>
+          <p className='text-md leading-8 py-5 text-gray-800'>
+            freelance web developer, i build websites and web apps for small businesses and individuals.
+          </p>
+        </div>
+        <div className='flex justify-center gap-16 text-gray-500 '>
+          <AiFillLinkedin className='text-4xl'/>
+          <AiFillTwitterCircle className='text-4xl'/>
+          <AiFillGithub className='text-4xl'/>
+        </div>
+        <div className='mt-10 flex justify-center mx-auto bg-gradient-to-t from-cyan-500 to-blue-500 rounded-full h-80 w-80 overflow-hidden'>
+          <Image  src={deved}  />
+        </div>
+      </section>
+      <section>
+      <div className='text-center'>
+            <h3 className="text-3xl py-1 text-gray-700 ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a full stack  developer , I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups  haha</span>
+            </p>
+           
+      </div>
+      <div className=''> 
+        <div className=' text-center shadow-xl p-10 rounded-xl my-10  flex-1 '>
+          <Image className='mx-auto' src={design}  width={100} height={100}/>
+          <h3 className='text-lg font-medium pt-8 pb-2 py-2'> Beautiful Designs </h3>
+          <p className='py-2'> I design and build beautiful websites and web apps</p>
+          <h4 className='py-4 text-lg text-teal-400'> Desgin tools I use </h4>
+          <p className='text-gray-800 py-1 '>Photoshop</p>
+          <p className='text-gray-800 py-1 '>Figma</p>
+          <p className='text-gray-800 py-1'>Adobe Xd</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+      </main>
+      
+    </div>
+    
   )
 }
