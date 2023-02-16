a9f176a2a8fd7d905a2d2549a382f534f1b849a9
AbdessamadPas <boughadoin@gmail.com>
2023-02-16 17:13:37 +0100

    integrate redux-toolkit and switch to the default app

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import Home from '../app/Home';
+import { store } from '../../store'
+import { Provider } from 'react-redux'
+import '../app/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+      <Provider store={store}>
+          <Home/>
+    </Provider>
+  );
+}
+
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    posts: [
+        {id: '1', title: 'Squirtle laid an egg', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.'},
+
+    ]}
+
+const rootReducer = (state= initState, action)=> {
+  return state
+}
+    
+
+export default rootReducer
